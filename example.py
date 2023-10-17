79673abedaba8f9a8e2f853f284c0b0a08d8de57
Hessam Mehr <hessammehr@gmail.com>
2023-10-17 16:02:19 +0100

    Format code with black.

--- example.py
+++ example.py
@@ -1,14 +1,17 @@
 from pico_ctrlaer import mux, ON, OFF, CtrlAer, always_on, inactive
 
+
 def prog1():
     for _ in range(300):
         yield ON, 50
         yield OFF, 70
 
+
 def prog2():
     for _ in range(200):
         yield ON, 66
         yield OFF, 155
+
 
 def prog3():
     for _ in range(50):
@@ -16,6 +19,7 @@ def prog3():
         yield OFF, 92
         yield ON, 100
         yield OFF, 130
+
 
 # Base pin compatible with Pimoroni Tiny RP2040
 progs = [prog1(), prog2(), prog1(), prog2(), prog1(), always_on, inactive]
--- pico_ctrlaer.py
+++ pico_ctrlaer.py
@@ -5,17 +5,21 @@ ON = const(1)
 OFF = const(0)
 FREQ = const(108_050)
 
+
 def _always_on():
     while True:
         yield ON, 1000
+
 
 def _inactive():
     # Yielding once is enough: mux() yields OFF when the generator is exhausted.
     # This guarantees that mux() terminates after the last active program.
     yield OFF, 1000
 
+
 inactive = _inactive()
 always_on = _always_on()
+
 
 def mux(progs):
     N = len(progs)
@@ -41,23 +45,29 @@ def mux(progs):
         for i in range(N):
             times[i] -= min_time
 
+
 def ticks(time):
     return time * FREQ // 1000
+
 
 class CtrlAer:
     def __init__(self, sm_number, base_pin, n_pins, freq=FREQ):
         self.n_pins = n_pins
         self.freq = freq
 
-        @asm_pio(out_init=(PIO.OUT_LOW,)*n_pins, out_shiftdir=PIO.SHIFT_RIGHT, fifo_join=PIO.JOIN_TX)
+        @asm_pio(
+            out_init=(PIO.OUT_LOW,) * n_pins,
+            out_shiftdir=PIO.SHIFT_RIGHT,
+            fifo_join=PIO.JOIN_TX,
+        )
         def oscillator():
-            pull() # pin states
+            pull()  # pin states
             mov(x, osr)
 
             out(pins, n_pins)[4]
             out(pins, n_pins)
 
-            pull() # duration in ticks (1 tick = 1/freq s)
+            pull()  # duration in ticks (1 tick = 1/freq s)
             mov(y, osr)
 
             label("loop")
@@ -66,7 +76,9 @@ class CtrlAer:
             out(pins, n_pins)
             jmp(y_dec, "loop")[1]
 
-        self.sm = StateMachine(sm_number, oscillator, freq=self.freq*2*5, out_base=Pin(base_pin))
+        self.sm = StateMachine(
+            sm_number, oscillator, freq=self.freq * 2 * 5, out_base=Pin(base_pin)
+        )
         self.sm.active(1)
 
     def run(self, prog):
