52099008b3680d5724d7140618f711a033b01412
Hessam Mehr <hessammehr@gmail.com>
2025-03-13 11:59:28 +0000

    Add listen functionality for command execution from input stream.

---
+++ examples/listen.py
@@ -0,0 +1,14 @@
+"""
+Listens to commands from stdin and executes them.
+The commands are expected to be in the format:
+<cmd>,<duration> where <cmd> is the command to be executed
+and <duration> is the duration in milliseconds.
+
+If the received command is 'END', listening stops and the function returns."""
+
+from ctrlaer import CtrlAer
+from sys import stdin
+
+ctrlaer = CtrlAer(sm_number=0, base_pin=0, n_pins=1, freq=5000)
+
+ctrlaer.listen(stdin)
